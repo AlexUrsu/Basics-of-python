00fca85177b37ba50b28654515a089f8df3aee09
Alexander Ursu <avursu@mail.ru>
2022-09-11 14:16:40 +0300

    Сделана домашняя работа по 1й лекции

---
+++ les1_ex1.py
@@ -0,0 +1,14 @@
+# Занятие №1 задание №1
+# Поработайте с переменными, создайте несколько, выведите на экран.
+
+f_name = 'Alexander'
+l_name = 'Vasilevich'
+sur_name = 'Ursu'
+print(f'Hello, my name is {f_name } {l_name} {sur_name}.')
+
+# Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+
+user_name = input('What is your name?')
+print('Nice to meet you', user_name+'!')
+user_age = input('How old are you?')
+print('Oh where are my', user_age, ':)')
